9e6ccc84af230264404388726ff837be3fea99dd
hergottwilliam <hergottwilliam@gmail.com>
2023-03-19 17:45:54 -0400

    slider now adjusts size of grid, but small red line appears sometimes (bug)

--- etchasketch.js
+++ etchasketch.js
@@ -1,13 +1,20 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "black";
+
+let currentSize = DEFAULT_SIZE;
+let currentColor = DEFAULT_COLOR;
+
 const gamebox = document.querySelector(".gamebox");
 
 let gamebox_height = gamebox.offsetHeight;
 let gamebox_width = gamebox.offsetWidth;
 
+// create a grid of size x size
 function createGrid(size){ // size = desired length of each side (size**2 == total pixels)
     // ex:
     // gamebox dimensions = 700px X 700px
     // pixel dimensions = 700px/size X 700px/size 
-    
+
     let total_pixels = size**2
     let grid_columns = "";
 
@@ -18,7 +25,6 @@ function createGrid(size){ // size = desired length of each side (size**2 == tot
 
         // style the pixel
         pixel.style.backgroundColor = "white";
-        pixel.style.border = "0px solid black";
         pixel.style.width = `${gamebox_width / size}px`;
         pixel.style.height = `${gamebox_height / size}px`;
 
@@ -31,18 +37,37 @@ function createGrid(size){ // size = desired length of each side (size**2 == tot
     }
     gamebox.style.gridGap = "0px";
     gamebox.style.gridTemplateColumns = grid_columns;
+
+    // select all the pixels that currently exist in grid
+    let pixels = document.querySelectorAll('.pixel');
+
+    pixels.forEach((pix) => {
+
+        pix.addEventListener("mouseover", function(e) {
+            e.target.style.backgroundColor = currentColor;
+        });
+    });
 }
 
-// grid size changes with number of pixels BUG
-createGrid(50);
+function clearGrid() {
+    // clear the grid
+    const deletePixels = document.querySelectorAll(".pixel");
+    for (let i = 0; i < deletePixels.length; i++) {
+        deletePixels[i].remove();
+    }
+}
 
-// select all the pixels that currently exist in grid
-const pixels = document.querySelectorAll('.pixel');
+// START OF APP, grid = 16x16 by default
+createGrid(DEFAULT_SIZE);
 
-// pixels change color when clicked
-pixels.forEach((pix) => {
+// select slider elements
+const slider = document.querySelector(".slider");
+const slider_value = document.querySelector(".slidervalue");
 
-    pix.addEventListener("mouseover", function(e) {
-        e.target.style.backgroundColor = "black";
-    });
+// slider changes size of grid
+slider.addEventListener("change", function() {
+    currentSize = slider.value;
+    slider_value.textContent = `${currentSize}x${currentSize}`;
+    clearGrid();
+    createGrid(currentSize);
 });
